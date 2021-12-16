0d281d37fa6ec5cb9fc96147cd0d1c1a951c1fd8
Mark Borysiak <borysiak.1@gmail.com>
2021-12-15 17:43:06 -0800

    converted covar to full class

--- Scripts/Simulation/zSim_Helper_Covar.py
+++ Scripts/Simulation/zSim_Helper_Covar.py
@@ -1,6 +1,7 @@
 #%%
 
 # core packages
+from os import stat
 from numpy.core.fromnumeric import size
 import pandas as pd
 import numpy as np
@@ -8,6 +9,7 @@ import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 import copy
+from collections import Counter
 
 # linear optimization
 from cvxopt import matrix
@@ -23,12 +25,13 @@ dm = DataManage(db_path)
 
 class FootballSimulation:
 
-    def __init__(self, dm, pos_require_start, week, set_year, num_iters):
+    def __init__(self, dm, week, set_year, salary_cap, pos_require_start, num_iters):
 
         self.week = week
-        self.year = set_year
+        self.set_year = set_year
         self.pos_require_start = pos_require_start
         self.num_iters = num_iters
+        self.salary_cap = salary_cap
 
         # pull in the player data (means, team, position) and covariance matrix
         player_data = dm.read('''SELECT * FROM Covar_Means''', 'Simulation')
@@ -78,8 +81,9 @@ class FootballSimulation:
         return predictions
 
     def init_select_cnts(self):
+        
         player_selections = {}
-        for p in self.player_data:
+        for p in self.player_data.player:
             player_selections[p] = 0
         return player_selections
 
@@ -102,7 +106,7 @@ class FootballSimulation:
     def add_players(self, to_add):
         
         h_player_add = {}
-        open_pos_require = copy.deepcopy(self.pos_require)
+        open_pos_require = copy.deepcopy(self.pos_require_start)
         df_add = self.player_data[self.player_data.player.isin(to_add)]
         for player, pos in df_add[['player', 'pos']].values:
             h_player_add[f'{player}'] = -1
@@ -132,6 +136,7 @@ class FootballSimulation:
         return df[~df.player.isin(to_drop)].reset_index(drop=True)
 
 
+    @staticmethod
     def player_matrix_mapping(df):
         idx_player_map = {}
         player_idx_map = {}
@@ -159,6 +164,7 @@ class FootballSimulation:
         return position_map
 
 
+    @staticmethod
     def team_matrix_mapping(df):
         
         team_map = {}
@@ -168,6 +174,7 @@ class FootballSimulation:
 
         return team_map
 
+    @staticmethod
     def create_A_position(position_map, player_map):
 
         num_positions = len(position_map)
@@ -181,16 +188,18 @@ class FootballSimulation:
 
         return A_positions
 
+    @staticmethod
     def create_b_matrix(pos_require):
         return np.array(list(pos_require.values())).reshape(-1,1)
 
+    @staticmethod
     def create_G_salaries(df):
         return df.salary.values.reshape(1, len(df))
 
-    def create_h_salaries(salary_cap):
-        return np.array(salary_cap).reshape(1, 1)
+    def create_h_salaries(self):
+        return np.array(self.salary_cap).reshape(1, 1)
 
-
+    @staticmethod
     def create_G_team(team_map, player_map):
         
         num_players = len(player_map)
@@ -207,11 +216,12 @@ class FootballSimulation:
         return G_teams
 
 
-    def get_max_team(labels, c_points, added_teams):
+    
+    def get_max_team(self, labels, c_points, added_teams):
 
         team_pts = pd.concat([labels, c_points], axis=1)
         team_pts = team_pts[~team_pts.pos.isin(['DEF'])]
-        team_pts = pd.merge(team_pts, top_team_players, on=['player', 'team'])
+        team_pts = pd.merge(team_pts, self.top_team_players, on=['player', 'team'])
         team_pts = team_pts.drop(['salary', 'player', 'pos'], axis=1)
         team_pts = team_pts.groupby('team').sum()
 
@@ -224,10 +234,10 @@ class FootballSimulation:
         return best_team
 
 
-    def create_h_teams(team_map, added_teams, set_max_team, min_players):
+    def create_h_teams(self, team_map, added_teams, set_max_team, min_players):
 
         if set_max_team is None: 
-            max_team = get_max_team(labels, c_points, added_teams)
+            max_team = self.get_max_team(self.labels, self.c_points, added_teams)
         else:
             max_team = set_max_team
 
@@ -236,6 +246,7 @@ class FootballSimulation:
 
         return h_teams, max_team
 
+    @staticmethod
     def create_G_players(player_map):
 
         num_players = len(player_map)
@@ -244,6 +255,7 @@ class FootballSimulation:
 
         return G_players
 
+    @staticmethod
     def create_h_players(player_map, h_player_add):
         num_players = len(player_map)
         h_players = np.zeros(shape=(num_players, 1))
@@ -253,6 +265,7 @@ class FootballSimulation:
 
         return h_players
 
+    @staticmethod
     def sample_c_points(data, max_entries):
 
         labels = data[['player', 'pos', 'team', 'salary']]
@@ -260,7 +273,7 @@ class FootballSimulation:
 
         return labels, current_points
 
-
+    @staticmethod
     def solve_ilp(c, G, h, A, b):
             
         # solve the integer LP problem
@@ -269,6 +282,7 @@ class FootballSimulation:
         return status, x
 
 
+    @staticmethod
     def tally_player_selections(predictions, player_selections, x):
             
             # find all LP results chosen and equal to 1
@@ -284,21 +298,29 @@ class FootballSimulation:
 
             return player_selections
 
+    
+    def final_results(self, player_selections):
+        results = pd.DataFrame(player_selections, index=['SelectionCounts']).T
+        results = results.sort_values(by='SelectionCounts', ascending=False).iloc[:25]
+        salaries = self.player_data[['player', 'salary']].set_index('player')
+        results = pd.merge(results, salaries, left_index=True, right_index=True)
+        results = results.reset_index().rename(columns={'index': 'player'})
+        results.SelectionCounts = 100*np.round(results.SelectionCounts / self.num_iters, 3)
+        return results
 
-    pos_require_start = {'QB': 1, 'RB': 2, 'WR': 3, 'TE': 1, 'DEF': 1}
-    num_runs = 500
+    @staticmethod
+    def final_team_cnts(max_team_cnt):
+        max_team_cnt = Counter(max_team_cnt)
+        df = pd.DataFrame(dict(max_team_cnt), index=[0]).T.reset_index()
+        df.columns=['team', 'high_score_team']
+        df = df.sort_values(by='high_score_team', ascending=False).reset_index(drop=True)
 
-    # set league information, included position requirements, number of teams, and salary cap
-    to_add = ["Ja'Marr Chase", 'Joe Burrow', 'Tee Higgins', 'George Kittle',
-            'Javonte Williams', 'Melvin Gordon', 'CLE', 'Gabriel Davis', 'Hunter Renfrow']
-    to_drop = ['Antonio Gibson']
-
-    min_players_same_team = 3
-    salary_cap = 50000
-    set_max_team = None
+        return df
 
     def run_sim(self, to_add, to_drop, min_players_same_team, set_max_team):
         
+        # can set as argument, but static set for now
+        num_options=500
         player_selections = self.init_select_cnts()
         max_team_cnt = []
 
@@ -315,31 +337,31 @@ class FootballSimulation:
                 cur_pos_require = self.add_flex(open_pos_require)
 
                 # get predictions and remove to drop players
-                predictions = self.get_predictions(num_options=500)
+                predictions = self.get_predictions(num_options=num_options)
                 predictions = self.drop_players(predictions, to_drop)
 
                 if i == 0:
                     position_map = self.position_matrix_mapping(cur_pos_require)
-                    idx_player_map, player_idx_map = player_matrix_mapping(predictions)
-                    team_map = team_matrix_mapping(predictions)
+                    idx_player_map, player_idx_map = self.player_matrix_mapping(predictions)
+                    team_map = self.team_matrix_mapping(predictions)
 
-                    A_position = create_A_position(position_map, idx_player_map)
-                    b_position = create_b_matrix(cur_pos_require)
+                    A_position = self.create_A_position(position_map, idx_player_map)
+                    b_position = self.create_b_matrix(cur_pos_require)
 
-                    G_salaries = create_G_salaries(predictions)
-                    h_salaries = create_h_salaries(salary_cap)
+                    G_salaries = self.create_G_salaries(predictions)
+                    h_salaries = self.create_h_salaries()
 
-                    G_players = create_G_players(player_idx_map)
-                    h_players = create_h_players(player_idx_map, h_player_add)
+                    G_players = self.create_G_players(player_idx_map)
+                    h_players = self.create_h_players(player_idx_map, h_player_add)
 
-                    G_teams = create_G_team(team_map, idx_player_map)
+                    G_teams = self.create_G_team(team_map, idx_player_map)
                 
             # generate the c matrix with the point values to be optimized
-            labels, c_points = sample_c_points(predictions, num_options)
+            self.labels, self.c_points = self.sample_c_points(predictions, num_options)
             
             if remaining_pos_cnt > min_players_same_team and max_added_team_cnt < min_players_same_team:
             
-                h_teams, max_team = create_h_teams(team_map, added_teams, set_max_team, min_players_same_team)
+                h_teams, max_team = self.create_h_teams(team_map, added_teams, set_max_team, min_players_same_team)
                 max_team_cnt.append(max_team)
                 G = np.concatenate([G_salaries, G_teams, G_players])
                 h = np.concatenate([h_salaries, h_teams, h_players])
@@ -352,25 +374,39 @@ class FootballSimulation:
             h = matrix(h, tc='d')
             b = matrix(b_position, tc='d')
             A = matrix(A_position, tc='d')    
-            c = matrix(c_points, tc='d')
+            c = matrix(self.c_points, tc='d')
 
             if len(to_add) < 9:
-                status, x = solve_ilp(c, G, h, A, b)
-                player_selections = tally_player_selections(predictions, player_selections, x)
+                status, x = self.solve_ilp(c, G, h, A, b)
+                player_selections = self.tally_player_selections(predictions, player_selections, x)
             
-        results = pd.DataFrame(player_selections,index=['SelectionCounts']).T
-        results.sort_values(by='SelectionCounts', ascending=False).iloc[:25]
+        results = self.final_results(player_selections)
+        team_cnts = self.final_team_cnts(max_team_cnt)
 
-
-
-
-
-
-
+        return results, team_cnts
 
 
 
 # %%
 
+week = 14
+year = 2021
+salary_cap = 50000
+pos_require_start = {'QB': 1, 'RB': 2, 'WR': 3, 'TE': 1, 'DEF': 1}
+num_iters = 500
 
+sim = FootballSimulation(dm, week, year, salary_cap, pos_require_start, num_iters)
+
+
+min_players_same_team = 3
+set_max_team = 'KC'
+to_add = ['Patrick Mahomes', 'Tyreek Hill', 'Clyde Edwards Helaire', 'Antonio Gibson',
+          'CLE', 'Austin Hooper', 'Jeff Wilson', 'Brandin Cooks', 'Cole Beasley']
+to_drop = ['Jared Cook']
+results, max_team_cnt = sim.run_sim(to_add, to_drop, min_players_same_team, set_max_team)
+results
+
+# %%
+
+max_team_cnt
 # %%
